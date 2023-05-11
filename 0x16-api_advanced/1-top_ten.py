d6340017adf040ac218c92bf5ecd50430a86e7e4
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-05-11 21:19:32 +0000

    Top ten posts per sub

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Query Reddit API for top ten posts per sub"""
+
+
+import requests
+import sys
+
+
+def top_ten(subreddit):
+    """Query reddit API for top ten posts per sub"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    try:
+        response = requests.get(url, headers=headers,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            children = response.json().get('data').get('children')
+            for i in range(10):
+                print(children[i].get('data').get('title'))
+        else:
+            print("None")
+    except Exception:
+        print("None")
