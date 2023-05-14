8b9470dd5e08f2f4d8158e3c943331d4b136013a
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-05-14 19:59:59 +0000

    File path again

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # Replace phpp extensions with php
 
 exec { 'replace-phpp':
-  command => 'sed -i s/phpp/php/g var/www/html/wp-settings.php',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
 
