81989fef5cbafcdaf6cc9226bbd24fc070b4dc05
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-05-18 12:06:38 +0000

    Bin path

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,10 +4,10 @@
 
 exec{'increase ulimit':
   command => 'sed -i "s/15/4096" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin'
+  path    => '/usr/local/bin/'
 }
 
 exec {'restart':
-  command => 'nginx restart',
+  command => 'service nginx restart',
   path    => '/etc/init.d/'
 }
