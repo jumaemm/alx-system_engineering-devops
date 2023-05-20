abb02522045df6d68169d08d27e5fb080b2c7f1c
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-05-20 17:22:11 +0000

    Increase file limits

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# This script increases file limits for loged in user holberton
+
+# Increase hard file limit
+exec { 'increase-hard-file':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit
+exec { 'increase-soft-file':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
+  path    => '/usr/local/bin/:/bin/'
+}
